43da1abdb70fe26971380989fa3bbe7545e2d79b
Ryan Bartolay <rbartolay.gotechsolutions@gmail.com>
2018-04-23 23:48:29 +0800

    brand crud api

--- Inventory/src/main/java/com/bartolay/inventory/controllers/BrandRestController.java
+++ Inventory/src/main/java/com/bartolay/inventory/controllers/BrandRestController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +32,7 @@ public class BrandRestController {
 	}
 
 	@RequestMapping(value="/api/brands/{id}", method=RequestMethod.GET)
-	public ResponseEntity<Brand> getById( @PathVariable Long id) {
+	public ResponseEntity<Brand> getById(@PathVariable Long id) {
 		try {
 			Brand brand = brandRepository.apiFindById(id);
 			return ResponseEntity.ok(brand);
@@ -39,14 +41,20 @@ public class BrandRestController {
 			return null;
 		}
 	}
-
-	public Brand create(Brand k) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	@RequestMapping(value="/api/brands", method=RequestMethod.POST)
+	public ResponseEntity<Brand> create(@RequestBody Brand brand) {
+		return ResponseEntity.ok(brandRepository.save(brand));
 	}
 
-	public Brand update(Brand k) {
-		// TODO Auto-generated method stub
-		return null;
+	@RequestMapping(value="/api/brands", method=RequestMethod.PUT)
+	public ResponseEntity<Brand> update(@RequestBody Brand brand) {
+		return ResponseEntity.ok(brandRepository.save(brand));
+	}
+	
+	@RequestMapping(value="/api/brands/{id}", method=RequestMethod.DELETE)
+	public BodyBuilder delete(@PathVariable Long id) {
+		brandRepository.deleteById(id);
+		return ResponseEntity.ok();
 	}
 }
