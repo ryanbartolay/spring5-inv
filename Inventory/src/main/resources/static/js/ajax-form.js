94bcd6028b17c095ab90543ad5ff8cc19ad24d2e
Ryan Bartolay <rbartolay.gotechsolutions@gmail.com>
2018-04-29 11:27:54 +0800

    form processing

--- Inventory/src/main/resources/static/js/ajax-form.js
+++ Inventory/src/main/resources/static/js/ajax-form.js
@@ -2,7 +2,13 @@ jQuery(function($) {
     $('form[data-async]').on('submit', function(event) {
         var $form = $(this);
         var $target = $($form.attr('data-target'));
-
+        var $error = $($form.find("#errors"));
+        var $btnSubmit = $($form.find("#submit"));
+        var $btnSubmitText = $btnSubmit.text();
+        
+        $btnSubmit.attr("disabled", "disabled");
+        $btnSubmit.html("Processing...");
+        
         $.ajax({
             type: $form.attr('method'),
             url: $form.attr('action'),
@@ -10,6 +16,20 @@ jQuery(function($) {
 
             success: function(data, status) {
                 $target.html(data);
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+            	var index;
+            	var errors = xhr.responseJSON.errors;
+            	
+            	var html = "<ul>";
+            	for (index = 0; index < errors.length; ++index) {
+            	    html += "<li>" +errors[index]+ "</li>";
+            	}
+            	html += "</ul>"
+            	
+            	$error.html(html);
+            	$btnSubmit.removeAttr("disabled");
+            	$btnSubmit.html($btnSubmitText);
             }
         });
 
