01cd5886bddb28c76a68e9f5d11cc3a247b64425
Ryan Bartolay <rbartolay.gotechsolutions@gmail.com>
2018-05-08 20:52:18 +0800

    opening stock

--- Inventory/src/main/java/com/bartolay/inventory/repositories/impl/OpeningStockDatatableRepositoryImpl.java
+++ Inventory/src/main/java/com/bartolay/inventory/repositories/impl/OpeningStockDatatableRepositoryImpl.java
@@ -1,10 +1,20 @@
 package com.bartolay.inventory.repositories.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Query;
+
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.bartolay.inventory.components.RepositoryComponent;
+import com.bartolay.inventory.datatable.model.DatatableColumn;
 import com.bartolay.inventory.datatable.model.DatatableParameter;
 import com.bartolay.inventory.repositories.DatatableRepository;
 
@@ -14,14 +24,67 @@ public class OpeningStockDatatableRepositoryImpl extends RepositoryComponent imp
 
 	@Override
 	public long findAllCount(DatatableParameter datatableParameter) {
-		// TODO Auto-generated method stub
-		return 0;
+		String SQL = "SELECT COUNT(id) FROM OpeningStock ";
+
+		if(datatableParameter.getSearch() != null) {
+			SQL += " WHERE  documentNumber like :documentNumber ";
+		}
+
+		Query query = em.createNativeQuery(SQL);
+		if(datatableParameter.getSearch() != null) {
+			query.setParameter("documentNumber", PERCENT + datatableParameter.getSearch() + PERCENT);
+		}
+		return Long.parseLong(query.getSingleResult().toString());
 	}
 
 	@Override
 	public JSONArray findAllData(DatatableParameter datatableParameter) {
-		// TODO Auto-generated method stub
-		return null;
+		try{
+			DatatableColumn sortColumn = datatableParameter.getSortColumn();
+			String SQL = "SELECT * FROM opening_stock ";
+			List<Object> SQL_PARAMS = new ArrayList<>();
+			
+			
+			if(datatableParameter.getSearch() != null) {
+				SQL += " WHERE document_number like ?";
+				SQL_PARAMS.add(datatableParameter.getSearch() + PERCENT);
+			}
+
+			// sort order by column
+			if(sortColumn != null && datatableParameter.getSortOrder() != null) {
+				SQL += " ORDER BY "+sortColumn.getData()+ " " + datatableParameter.getSortOrder().name();	
+			}
+			
+			if(datatableParameter.getLength() > 0) {
+				SQL += " LIMIT " + datatableParameter.getLength() + " OFFSET " + datatableParameter.getStart();
+			}
+			
+			List<JSONObject> domains = new ArrayList<>();
+			
+			domains = jdbcTemplate.query(SQL, SQL_PARAMS.toArray(), new RowMapper<JSONObject>() {
+
+				@Override
+				public JSONObject mapRow(ResultSet rs, int arg1) throws SQLException {
+					JSONObject obj = new JSONObject();
+					obj.put("id", rs.getLong("id"));
+					obj.put("system_number", rs.getString("system_number"));
+					obj.put("document_number", rs.getString("document_number"));
+					obj.put("description", rs.getString("description"));
+					return obj;
+				}
+			});
+			
+			JSONArray array = new JSONArray();
+			
+			domains.forEach(domain -> {
+				array.put(domain);
+			});
+			return array;
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}	
 	}
 
 }
--- Inventory/src/main/java/com/bartolay/inventory/services/impl/OpeningStockServiceImpl.java
+++ Inventory/src/main/java/com/bartolay/inventory/services/impl/OpeningStockServiceImpl.java
@@ -6,17 +6,25 @@ import java.util.Map;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bartolay.inventory.datatable.model.DatatableParameter;
 import com.bartolay.inventory.entity.stock.OpeningStock;
 import com.bartolay.inventory.form.OpeningStockForm;
+import com.bartolay.inventory.repositories.DatatableRepository;
+import com.bartolay.inventory.repositories.impl.CategoryDataTableRepositoryImpl;
 import com.bartolay.inventory.services.OpeningStockService;
 
 @Service
 @Transactional
 public class OpeningStockServiceImpl implements OpeningStockService {
 
+	@Autowired
+	private DatatableRepository categoryDataTableRepository;
+	
 	@Override
 	public List<OpeningStock> findAll() {
 		// TODO Auto-generated method stub
@@ -25,8 +33,17 @@ public class OpeningStockServiceImpl implements OpeningStockService {
 
 	@Override
 	public JSONObject retrieveDatatableList(Map<String, String> requestMap) {
-		// TODO Auto-generated method stub
-		return null;
+		DatatableParameter parameter = new DatatableParameter(requestMap);
+		JSONArray array = categoryDataTableRepository.findAllData(parameter);
+		long recordsTotal = categoryDataTableRepository.findAllCount(parameter);
+		
+		JSONObject object = new JSONObject();
+		object.put("data", array);
+		object.put("recordsTotal", recordsTotal);
+		object.put("recordsFiltered", recordsTotal);
+		object.put("draw", parameter.getDraw());
+		
+		return object;
 	}
 
 	@Override
